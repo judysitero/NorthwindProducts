db3d09c111293e883f6344050a29a8a0fbaa2a28
Judith Sitero <jsitero@my.yearupunited.org>
2025-12-02 15:44:26 -0800

    test

--- src/main/java/com/pluralsight/NorthwindDBConnection.java
+++ src/main/java/com/pluralsight/NorthwindDBConnection.java
@@ -1,4 +1,30 @@
 package com.pluralsight;
 
+import java.sql.*;
+
 public class NorthwindDBConnection {
+    public static void main(String[] args) {
+        try {
+
+            // Step 1: Open the connection
+            Connection connection = DriverManager.getConnection("jdbc:mysql: //localhost:3306/northwind", "root", "yearup");
+
+            // Step 2: Create a statement (query holder)
+            Statement statement = connection.createStatement();
+
+            // Step 3a: execute the query
+            String query = "SELECT * FROM products";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            //Step 3b: process the result
+            while(resultSet.next()) {
+                System.out.println(resultSet.getString("ProductName"));
+            }
+
+            // Step 4: we close the connection
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
